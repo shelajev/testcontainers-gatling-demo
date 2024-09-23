3e9d4e977d6a67a8bca4fe4ed7b5c73c6c5ccf72
shelajev <shelajev@gmail.com>
2024-09-23 15:52:56 +0300

    containerized app

--- src/test/java/com/testcontainers/catalog/ContainersConfig.java
+++ src/test/java/com/testcontainers/catalog/ContainersConfig.java
@@ -3,6 +3,7 @@ package com.testcontainers.catalog;
 import static org.testcontainers.utility.DockerImageName.parse;
 
 import com.testcontainers.catalog.domain.FileStorageService;
+import net.bytebuddy.asm.Advice;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -11,33 +12,40 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.MountableFile;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainersConfig {
 
+    static Network network = Network.newNetwork();
+
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
         PostgreSQLContainer<?> selfPostgreSQLContainer =
-                new PostgreSQLContainer<>(parse("postgres:16-alpine"));
-
-
+                new PostgreSQLContainer<>(parse("postgres:16-alpine"))
+                        .withNetwork(network).withNetworkAliases("postgres");
         return selfPostgreSQLContainer;
     }
 
     @Bean
     @ServiceConnection
     KafkaContainer kafkaContainer() {
-        return new KafkaContainer(parse("confluentinc/cp-kafka:7.5.0")).withReuse(true);
+        return new KafkaContainer(parse("confluentinc/cp-kafka:7.5.0"))
+                .withReuse(true).withNetwork(network).withNetworkAliases("kafka");
+
     }
 
     @Bean("localstackContainer")
     LocalStackContainer localstackContainer(DynamicPropertyRegistry registry) {
-        LocalStackContainer localStack = new LocalStackContainer(parse("localstack/localstack:2.3")).withReuse(true);
+        LocalStackContainer localStack = new LocalStackContainer(parse("localstack/localstack:2.3"))
+                .withNetwork(network).withNetworkAliases("localstack").withReuse(true);
         registry.add("spring.cloud.aws.credentials.access-key", localStack::getAccessKey);
         registry.add("spring.cloud.aws.credentials.secret-key", localStack::getSecretKey);
         registry.add("spring.cloud.aws.region.static", localStack::getRegion);
