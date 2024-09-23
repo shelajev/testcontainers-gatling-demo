decaf6383470e9095a2c3d93643f6cf8a19fdcf7
shelajev <shelajev@gmail.com>
2024-09-23 16:47:33 +0300

    fixe the ports!

--- src/test/java/com/testcontainers/TestMain.java
+++ src/test/java/com/testcontainers/TestMain.java
@@ -12,15 +12,17 @@ import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.testcontainers.utility.DockerImageName.parse;
 
 public class TestMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Network network = Network.newNetwork();
 
         PostgreSQLContainer<?> postgres =
@@ -35,12 +37,13 @@ public class TestMain {
 
 
         Path dockerfile = Paths.get("Dockerfile");
+
+        ;
+
         GenericContainer<?> app = new GenericContainer<>(new ImageFromDockerfile("gatling-demo-app", false)
                 .withFileFromPath("Dockerfile", Paths.get("Dockerfile"))
                 .withFileFromPath("target/java-local-development-workshop-0.0.1-SNAPSHOT.jar", Paths.get("target/java-local-development-workshop-0.0.1-SNAPSHOT.jar"))
         )
-
-                .withExposedPorts(8080)
                 .withEnv("SPRING_KAFKA_BOOTSTRAP_SERVERS", "BROKER://kafka:9092")
                 .withEnv("SPRING_DATASOURCE_URL", "jdbc:postgresql://postgres:5432/test")
                 .withEnv("SPRING_DATASOURCE_USERNAME", "test")
@@ -49,19 +52,22 @@ public class TestMain {
                 .withEnv("SPRING_CLOUD_AWS_CREDENTIALS_SECRET-KEY", localStack.getSecretKey())
                 .withEnv("SPRING_CLOUD_AWS_REGION_STATIC", localStack.getRegion())
                 .withEnv("SPRING_CLOUD_AWS_ENDPOINT", "localstack:4566")
-
+                .withExposedPorts(8080)
                 .withNetwork(network)
-                .waitingFor(Wait.forHttp("/actuator/health"))
-                .withCreateContainerCmdModifier(createContainerCmd -> {
-                            var hostConfig = new HostConfig();
-                            hostConfig.withMemory(1 * 1024L * 1024L);
-                            hostConfig.withCpuCount(1L);
-                            createContainerCmd.withHostConfig(hostConfig);
-                        }
-                );
+                .waitingFor(Wait.forHttp("/actuator/health"));
+
+//                .withCreateContainerCmdModifier(createContainerCmd -> {
+//                            var hostConfig = new HostConfig();
+//                            hostConfig.withMemory(1024L * 1024L * 1024L);
+//                            hostConfig.withCpuCount(1L);
+//                            createContainerCmd.withHostConfig(hostConfig);
+//                        }
+//                );
 
         Startables.deepStart(postgres, kafka, localStack).join();
+        localStack.execInContainer("awslocal", "s3api", "create-bucket", "--bucket product-images");
 
+        app.setPortBindings(List.of("8080:8080"));
         app.start();
 
         CloudflaredContainer cloudflaredContainer = new CloudflaredContainer(parse("cloudflare/cloudflared"), app.getMappedPort(8080));
